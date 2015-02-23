285819c4b69f54149dfaaf7692ec648cb6228406
btopro <bto108@psu.edu>
2015-02-23 16:49:30 -0500

    cleaning up structure more

--- template.php
+++ template.php
@@ -44,42 +44,46 @@ function foundation_access_menu_link__cis_service_connection_active_outline($var
   }
   $id = 'zfa-menu-panel-' . $element['#original_link']['mlid'];
   //$output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  $output = $element['#title'];
   // account for no link'ed items
   if ($element['#href'] == '<nolink>') {
-    $output = '<a href="#">' . $output . '</a>';
+    $output = '<a href="#">' . $element['#title'] . '</a>';
   }
   // account for sub menu things being rendered differently
   if (empty($sub_menu)) {
     // ending elements
-    $return .= '<li><a href="' . $element['#href'] . '"><div class="icon-assignment-white outline-nav-icon" data-grunticon-embed></div>' . $output . '</a></li>';
+    $options = $element['#localized_options'];
+    $options['html'] = TRUE;
+    $return .= '<li>' . l('<div class="icon-assignment-white outline-nav-icon" data-grunticon-embed></div>' . $element['#title'], $element['#href'], $options) . '</li>';
   }
   else {
     if ($element['#original_link']['p3'] == 0) {
-      $short = preg_replace('/[^a-zA-Z0-9\s]/', '', strtolower($output));
+      if (empty($element['#attributes']['class'])) {
+        $element['#attributes']['class'] = array();
+      }
+      $short = preg_replace('/[^a-zA-Z0-9\s]/', '', strtolower($element['#title']));
       $return .= '<section role="tabpanel" aria-hidden="true" class="content" id="' . $short . '-panel">
-      <ul class="off-canvas-list content-outline-navigation">
-      <h2>' . $output . '</h2>
-      <h3>A long time ago, in science</h3>' . $sub_menu . "</ul>\n</section>";
+      <ul class="off-canvas-list content-outline-navigation ' . implode(' ', $element['#attributes']['class']) . '">
+      <h2>' . $element['#title'] . '</h2>
+      <h3></h3>' . $sub_menu . "</ul>\n</section>";
+      // @todo fill the H3 with something meaningful
     }
     elseif ($element['#original_link']['p4'] == 0) {
-      $return .= '<li class="has-submenu"><a href="#"><div class="icon-content-white outline-nav-icon" data-grunticon-embed></div><span class="outline-nav-text">' . $output . '</span></a>
+      $return .= '<li class="has-submenu"><a href="#"><div class="icon-content-white outline-nav-icon" data-grunticon-embed></div><span class="outline-nav-text">' . $element['#title'] . '</span></a>
         <ul class="left-submenu level-1-sub">
-          <h2>' . $output . '</h2>
-          <h3>Lets do Science!</h3>
+          <h2>' . $element['#title'] . '</h2>
+          <h3></h3>
           <li class="back">
-          <a href="#">Back to all Lessons</a></li>' . $sub_menu .'</ul></li>';
+          <a href="#">' . t('Back') . '</a></li>' . $sub_menu . '</ul></li>';
+      // @todo fill the H3 with something meaningful
     }
-    elseif ($element['#original_link']['p5'] == 0) {
-      $return .= '<li><label>' . $output . '</label></li>
-        <li class="has-submenu"><a href="' . $element['#href'] .'">Link 2 w/ submenu</a>
+    else {
+      $return .= '<li class="has-submenu">' . l($element['#title'], $element['#href'], $element['#localized_options']) . '
             <ul class="left-submenu">
-                <li class="back"><a href="#">Back</a></li>
+                <li class="back"><a href="#">' . t('Back') . '</a></li>
                 ' . $sub_menu . '
             </ul>
-        </li>
-        <li><a href="#">...</a></li>';
-      }
+        </li>';
+    }
   }
   return $return;
 }
